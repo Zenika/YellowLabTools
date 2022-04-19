4ca1094c01176922961c9155227866cea44356fc
Damien-Ar <damien.aranda42@gmail.com>
2022-04-19 18:03:26 +0200

    implement sources as file

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,9 @@ var debug       = require('debug')('ylt:cli');
 var meow        = require('meow');
 var path        = require('path');
 var EasyXml     = require('easyxml');
+var Q           = require('q');
+var fs          = require('fs');
+var {parse}     = require('yaml')
 
 var ylt         = require('../lib/index');
 
@@ -38,100 +41,129 @@ if (cli.input.length < 1) {
     cli.showHelp();
 }
 
-var url = cli.input[0];
-
-var options = {};
-
-// Screenshot option
-var screenshot = cli.flags.screenshot;
-if (screenshot && (typeof screenshot !== 'string' || screenshot.toLowerCase().indexOf('.png', screenshot.length - 4) === -1)) {
-    console.error('Incorrect parameters: screenshot must be a path that ends with ".png"');
-    process.exit(1);
+var input = cli.input[0];
+const urls = [];
+if(input && typeof input === 'string' && (input.toLowerCase().indexOf('.yml', input.length - 4) !== -1 || input.toLowerCase().indexOf('.yaml', input.length - 5) !== -1)){
+    const fileBuffer = fs.readFileSync(input)
+    const actions = parse(fileBuffer.toString())
+    urls.push(...actions.map(action => {
+        return {
+            url: action.url,
+            name: action.name,
+            // remove null or undefined
+            ...Object.fromEntries(Object.entries(checkOptions(action)).filter(([_, v]) => v != null))
+        }
+    }))
 }
-if (screenshot) {
-    if (path.resolve(screenshot) !== path.normalize(screenshot)) {
-        // It is not an absolute path, so it is relative to the current command-line directory
-        screenshot = path.join(process.cwd(), screenshot);
+else {
+    urls.push({url: input})
+}
+
+function checkOptions(source){
+    var options = {};
+    
+    // Screenshot option
+    var screenshot = source.screenshot;
+    if (screenshot && (typeof screenshot !== 'string' || screenshot.toLowerCase().indexOf('.png', screenshot.length - 4) === -1)) {
+        console.error('Incorrect parameters: screenshot must be a path that ends with ".png"');
+        process.exit(1);
     }
-    options.screenshot = cli.flags.screenshot;
+    if (screenshot) {
+        if (path.resolve(screenshot) !== path.normalize(screenshot)) {
+            // It is not an absolute path, so it is relative to the current command-line directory
+            screenshot = path.join(process.cwd(), screenshot);
+        }
+        options.screenshot = source.screenshot;
+    }
+
+    // Device simulation
+    options.device = source.device || 'mobile';
+
+    // Wait for CSS selector
+    options.waitForSelector = source.waitForSelector || null;
+
+    // Proxy
+    options.proxy = source.proxy || null;
+
+    // Cookie
+    options.cookie = source.cookie || null;
+
+    // HTTP basic auth
+    options.authUser = source.authUser || null;
+    options.authPass = source.authPass || null;
+
+    // Domain blocking
+    options.blockDomain =  source.blockDomain || null;
+    options.allowDomain =  source.allowDomain || null;
+    options.noExternals =  source.noExternals || null;
+
+    // Storage injection
+    options.localStorage = source.localStorage;
+    options.sessionStorage = source.sessionStorage;
+
+    // Output format
+    if (source.reporter && source.reporter !== 'json' && source.reporter !== 'xml') {
+        console.error('Incorrect parameters: reporter has to be "json" or "xml"');
+        process.exit(1);
+    }
+    return options
 }
 
-// Device simulation
-options.device = cli.flags.device || 'mobile';
+const globalOptions = checkOptions(cli.flags)
 
-// Wait for CSS selector
-options.waitForSelector = cli.flags.waitForSelector || null;
+// Display data
+function display(data) {
+    switch(cli.flags.reporter) {
+        case 'xml':
+            var serializer = new EasyXml({
+                manifest: true
+            });
 
-// Proxy
-options.proxy = cli.flags.proxy || null;
+            // Remove some heavy parts of the results object
+            delete data.toolsResults;
+            delete data.javascriptExecutionTree;
 
-// Cookie
-options.cookie = cli.flags.cookie || null;
+            var xmlOutput = serializer.render(data);
 
-// HTTP basic auth
-options.authUser = cli.flags.authUser || null;
-options.authPass = cli.flags.authPass || null;
+            // Remove special chars from XML tags: # [ ]
+            xmlOutput = xmlOutput.replace(/<([^>]*)#([^>]*)>/g, '<$1>');
+            xmlOutput = xmlOutput.replace(/<([^>]*)\[([^>]*)>/g, '<$1>');
+            xmlOutput = xmlOutput.replace(/<([^>]*)\]([^>]*)>/g, '<$1>');
 
-// Domain blocking
-options.blockDomain =  cli.flags.blockDomain || null;
-options.allowDomain =  cli.flags.allowDomain || null;
-options.noExternals =  cli.flags.noExternals || null;
+            // Remove special chars from text content: \n \0
+            xmlOutput = xmlOutput.replace(/(<[a-zA-Z]*>[^<]*)\n([^<]*<\/[a-zA-Z]*>)/g, '$1$2');
+            xmlOutput = xmlOutput.replace(/\0/g, '');
+            xmlOutput = xmlOutput.replace(/\uFFFF/g, '');
+            xmlOutput = xmlOutput.replace(/\u0002/g, '');
 
-// Storage injection
-options.localStorage = cli.flags.localStorage;
-options.sessionStorage = cli.flags.sessionStorage;
-
-// Output format
-if (cli.flags.reporter && cli.flags.reporter !== 'json' && cli.flags.reporter !== 'xml') {
-    console.error('Incorrect parameters: reporter has to be "json" or "xml"');
-    process.exit(1);
+            console.log(xmlOutput);
+            break;
+        default:
+            console.log(JSON.stringify(data, null, 2));
+    }
 }
 
 
-(function execute(url, options) {
+(function execute(urls, options) {
     'use strict';
-
-    ylt(url, options).
-
-        then(function(data) {
-
-            debug('Success');
-            switch(cli.flags.reporter) {
-                case 'xml':
-                    var serializer = new EasyXml({
-                        manifest: true
-                    });
-
-                    // Remove some heavy parts of the results object
-                    delete data.toolsResults;
-                    delete data.javascriptExecutionTree;
-
-                    var xmlOutput = serializer.render(data);
-
-                    // Remove special chars from XML tags: # [ ]
-                    xmlOutput = xmlOutput.replace(/<([^>]*)#([^>]*)>/g, '<$1>');
-                    xmlOutput = xmlOutput.replace(/<([^>]*)\[([^>]*)>/g, '<$1>');
-                    xmlOutput = xmlOutput.replace(/<([^>]*)\]([^>]*)>/g, '<$1>');
-
-                    // Remove special chars from text content: \n \0
-                    xmlOutput = xmlOutput.replace(/(<[a-zA-Z]*>[^<]*)\n([^<]*<\/[a-zA-Z]*>)/g, '$1$2');
-                    xmlOutput = xmlOutput.replace(/\0/g, '');
-                    xmlOutput = xmlOutput.replace(/\uFFFF/g, '');
-                    xmlOutput = xmlOutput.replace(/\u0002/g, '');
-
-                    console.log(xmlOutput);
-                    break;
-                default:
-                    console.log(JSON.stringify(data, null, 2));
-            }
-
-        }).fail(function(err) {
-            
-            debug('Test failed for %s', url);
-            console.error(err);
-            
-        });
+    urls.reduce((acc, {url, ...urlOptions}) => {
+            return acc.then((results) => {
+                return ylt(url, {...options, ...urlOptions}).then(data => {
+                    results.push(data)
+                    return results
+                })
+            })
+        }, Q([]))
+        .then(data => {
+                debug("All urls have been tested")
+                display(data)
+            })
+            .fail(err => {
+                debug("Error while testing urls")
+                debug(err)
+                console.log(err)
+            })
 
     debug('Test launched...');
 
-})(url, options);
+})(urls, globalOptions);
